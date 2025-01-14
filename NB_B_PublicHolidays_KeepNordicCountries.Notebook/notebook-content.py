3e78f8599a5dfc0ab8787865d99e86a614237328
MYH <37788058+myh-st@users.noreply.github.com>
2025-01-14 11:56:41 +0700

    Committing 9 items from workspace a856c2fa-e089-4412-a3a8-09159857efb6

---
+++ NB_B_PublicHolidays_KeepNordicCountries.Notebook/notebook-content.py
@@ -0,0 +1,41 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "9f9a5f74-da62-4803-82fc-752120328818",
+# META       "default_lakehouse_name": "DF_LH_Bronze",
+# META       "default_lakehouse_workspace_id": "a856c2fa-e089-4412-a3a8-09159857efb6"
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM DF_LH_Bronze.publicholidays LIMIT 1000")
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
